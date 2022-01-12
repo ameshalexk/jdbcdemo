6a4b08e161fbb7e9959520e13df2ca2241b28c29
ameshalexk <ameshalexusa@gmail.com>
2022-01-12 10:54:58 -0600

    Main scanner for welcome screen implemented

--- JDBCProject/src/com/cognixia/jump/main/MainRunner.java
+++ JDBCProject/src/com/cognixia/jump/main/MainRunner.java
@@ -1,9 +1,33 @@
 package com.cognixia.jump.main;
 
+import java.util.Scanner;
+
 public class MainRunner {
 
 	public static void main(String[] args) {
-		System.out.println("HI Amesh");
+		System.out.println("Employee Portal v1.0");
+		
+		System.out.println("Please enter 1 to enter the program and 2 to exit the program.");
+		
+		Scanner enterProgram = new Scanner(System.in);
+		
+		String ans = enterProgram.nextLine();
+				
+		switch (ans) {
+		  case "1":
+		    System.out.println("Welcome!");
+		    enterProgram.close();
+		    break;
+		  case "2":
+		    System.out.println("Bye!!!");
+		    enterProgram.close();
+		    break;
+		  default:
+		    System.out.println("Invalid Input....exiting program!!!");
+		    enterProgram.close();
+		}
+		
+		
 		
 	}
 
